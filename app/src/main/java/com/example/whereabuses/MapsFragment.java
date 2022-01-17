a127ed62163737cadd0f39427523c6321d24ac91
melo <mjrmo2@iscte-iul.pt>
2022-01-17 16:02:33 +0000

    try catch

--- app/src/main/java/com/example/whereabuses/MapsFragment.java
+++ app/src/main/java/com/example/whereabuses/MapsFragment.java
@@ -231,21 +231,25 @@ public class MapsFragment extends Fragment {
         Runnable runnable =
                 new Runnable(){
                     public void run(){
-                        rootRef.collection("731").document("Buses")
-                                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                            @Override
-                            public void onSuccess(@NonNull DocumentSnapshot documentSnapshot) {
-                                if(documentSnapshot.exists()){
-                                    GeoPoint geoPoint = (GeoPoint) documentSnapshot.get("731-1");
-                                    LatLng location = new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude());
-                                    System.out.println("FUI BUSCAR ESTA LOCALIZAÇAO" + location);
-                                    updateCarLocation(location);
+                        try {
+                            rootRef.collection("731").document("Buses")
+                                    .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                @Override
+                                public void onSuccess(@NonNull DocumentSnapshot documentSnapshot) {
+                                    if(documentSnapshot.exists()){
+                                        GeoPoint geoPoint = (GeoPoint) documentSnapshot.get("731-1");
+                                        LatLng location = new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude());
+                                        System.out.println("FUI BUSCAR ESTA LOCALIZAÇAO" + location);
+                                        updateCarLocation(location);
+                                    }
+
                                 }
+                            });
 
-                            }
-                        });
-
-                        handler.postDelayed(this, 20000);
+                            handler.postDelayed(this, 20000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 };
         handler.postDelayed(runnable,1000);
