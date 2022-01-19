0ef55aadfd9838dae3a9d853abca27c92b173ba4
ldasv <ldasv@iscte-iul.pt>
2022-01-19 20:37:04 +0000

    Chat room com erro 2

---
+++ app/src/main/java/com/example/whereabuses/ChatActivity.java
@@ -0,0 +1,114 @@
+package com.example.whereabuses;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class ChatActivity extends AppCompatActivity {
+    private Button addRoom;
+    private EditText room_name;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_Rooms = new ArrayList<>();
+    private String name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        addRoom = (Button) findViewById(R.id.btn_add_room);
+        room_name = (EditText) findViewById(R.id.msg_input);
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list_Rooms);
+        listView.setAdapter(arrayAdapter);
+        request_user_name();
+        addRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String,Object> map = new HashMap<String,Object>();
+                map.put(room_name.getText().toString(),"");
+                root.updateChildren(map);
+
+            }
+        });
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = snapshot.getChildren().iterator();
+                while(i.hasNext()){
+                        set.add(((DataSnapshot)i.next()).getKey());
+                }
+                list_Rooms.clear();
+                list_Rooms.addAll(set);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(),Chat_Room.class);
+                intent.putExtra("room_name",((TextView)view).getText().toString());
+                intent.putExtra("user_name ",name);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void  request_user_name() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter name:");
+        EditText input_field = new EditText(this);
+        builder.setView(input_field);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    name = input_field.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                request_user_name();
+            }
+        });
+        builder.show();
+
+    }
+
+}
